3550af33e6c292eed0bf00564e4ce7b01343a6b8
levisingularity <rafael.levi@singularitynet.io>
2024-02-21 17:38:47 -0300

    Setup OpenFaaS server script

--- install-openfaas.sh
+++ install-openfaas.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
-cd ~
+USER_NAME="dasadmin"
 
-#installing open-faas
-cat <<EOF > install-openfaas.sh
+function user_setup() {
+    adduser $USER_NAME
+    usermod -a -G sudo $USER_NAME
+    su $USER_NAME
+}
+
+function openfaas_setup() {
+    cat <<EOF >install-openfaas.sh
 #!/bin/bash
 set -ex
 git clone https://github.com/openfaas/faasd --depth=1
@@ -13,33 +19,39 @@ cd faasd
 mkdir -p /var/lib/faasd/.docker/
 EOF
 
-chmod +x install-openfaas.sh
-./install-openfaas.sh >> install-openfaas.log 2>&1
+    chmod +x install-openfaas.sh
+    ./install-openfaas.sh >>install-openfaas.log 2>&1
+}
 
-#installing docker
-cat <<EOF > install-docker.sh
-#!/bin/bash
-set -ex
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
-usermod -aG docker root
-systemctl enable docker
-systemctl start docker
-EOF
+function docker_setup() {
+    sudo apt-get update
+    sudo apt install net-tools ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    echo \
+        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo docker run hello-world
+    sudo docker rm $(sudo docker ps -a | grep hello | cut -d" " -f1)
+    sudo docker rmi hello-world
+    sudo usermod -a -G docker $USER_NAME
+}
 
-chmod +x install-docker.sh
-./install-docker.sh >> install-docker.log 2>&1
+function firewall_setup() {
+    if ! command -v ufw &>/dev/null; then
+        sudo apt-get update
+        sudo apt-get install ufw
+    fi
 
-#installing aws-cli
-cat <<EOF > install-aws-cli.sh
-#!/bin/bash
-snap install aws-cli --classic
-EOF
+    sudo ufw enable
+    sudo ufw allow 8080/tcp
+}
 
-chmod +x install-aws-cli.sh
-# ./install-aws-cli.sh >> install-aws-cli.log 2>&1
-
+user_setup
+docker_setup
+openfaas_setup
+firewall_setup
