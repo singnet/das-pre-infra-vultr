27e6cb31287a4af172d64866165ccfdf126cb16c
levisingularity <rafael.levi@singularitynet.io>
2024-02-21 17:38:31 -0300

    Setup mongoDB server script

--- install-mongodb.sh
+++ install-mongodb.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 set -e
 
-apt-get install gnupg curl >> ~/install-mongo.log 2>&1
+USER_NAME="dasadmin"
+MONGO_VERSION="7.0.5"
+MONGO_PORT="28100"
 
-curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
-   gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
-   --dearmor >> ~/install-mongo.log 2>&1
+function user_setup() {
+   adduser $USER_NAME
+   usermod -a -G sudo $USER_NAME
+   su $USER_NAME
+}
 
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list >> ~/install-mongo.log 2>&1
+function docker_setup() {
+   sudo apt-get update
+   sudo apt install net-tools ca-certificates curl gnupg
+   sudo install -m 0755 -d /etc/apt/keyrings
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+   sudo chmod a+r /etc/apt/keyrings/docker.gpg
+   echo \
+      "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+      sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+   sudo apt-get update
+   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+   sudo docker run hello-world
+   sudo docker rm $(sudo docker ps -a | grep hello | cut -d" " -f1)
+   sudo docker rmi hello-world
+   sudo usermod -a -G docker $USER_NAME
+}
 
-apt-get update >> ~/install-mongo.log 2>&1
+function mongo_setup() {
+   docker image pull mongo:$MONGO_VERSION
 
-apt-get install -y mongodb-org >> ~/install-mongo.log 2>&1
+   docker run --name mongo --restart always -p ${MONGO_PORT}:27017 mongo:$MONGO_VERSION
+}
 
-systemctl start mongod >> ~/install-mongo.log 2>&1
-
-systemctl status mongod >> ~/install-mongo.log 2>&1
-
-systemctl enable mongod >> ~/install-mongo.log 2>&1
+user_setup
+docker_setup
+mongo_setup
