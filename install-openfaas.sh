58e8b872bc3d4adb9a7e83012c44dde908df5bff
levisingularity <rafael.levi@singularitynet.io>
2024-02-21 22:00:03 -0300

    Update openfaas install

--- install-openfaas.sh
+++ install-openfaas.sh
@@ -4,9 +4,8 @@ set -e
 USER_NAME="dasadmin"
 
 function user_setup() {
-    adduser $USER_NAME
-    usermod -a -G sudo $USER_NAME
-    su $USER_NAME
+    adduser --disabled-password --gecos "" "$USER_NAME"
+    usermod -a -G $USER_NAME $USER_NAME
 }
 
 function openfaas_setup() {
@@ -24,31 +23,36 @@ EOF
 }
 
 function docker_setup() {
-    sudo apt-get update
-    sudo apt install net-tools ca-certificates curl gnupg
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
-    echo \
-        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
-        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo docker run hello-world
-    sudo docker rm $(sudo docker ps -a | grep hello | cut -d" " -f1)
-    sudo docker rmi hello-world
-    sudo usermod -a -G docker $USER_NAME
+    apt-get -y update
+    apt-get -y install net-tools ca-certificates curl gnupg
+
+    if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
+        install -m 0755 -d /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        chmod a+r /etc/apt/keyrings/docker.gpg
+        echo \
+            "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+            tee /etc/apt/sources.list.d/docker.list >/dev/null
+    fi
+
+    apt-get -y update
+    apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    dockerd &
+    docker run hello-world
+    docker rm $(docker ps -a | grep hello | cut -d" " -f1)
+    docker rmi hello-world
 }
 
 function firewall_setup() {
     if ! command -v ufw &>/dev/null; then
-        sudo apt-get update
-        sudo apt-get install ufw
+        apt-get update
+        apt-get install ufw
     fi
 
-    sudo ufw enable
-    sudo ufw allow 8080/tcp
+    ufw -y enable
+    ufw allow 8080/tcp
+    ufw allow ssh
 }
 
 user_setup
