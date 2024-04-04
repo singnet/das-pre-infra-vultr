57698b7e17e7ca593e025c071473b46c04345d3a
levisingularity <rafael.levi@singularitynet.io>
2024-04-04 11:00:26 -0300

    Set multiversion flag

--- install-apt-repository.sh
+++ install-apt-repository.sh
@@ -87,7 +87,7 @@ calculate_hash() {
 update_repository() {
     cd "\$1"
 
-    dpkg-scanpackages --arch amd64 pool/ > dists/stable/main/binary-amd64/Packages
+    dpkg-scanpackages --multiversion --arch amd64 pool/ > dists/stable/main/binary-amd64/Packages
     cat dists/stable/main/binary-amd64/Packages | gzip -9 > dists/stable/main/binary-amd64/Packages.gz
 
     cd dists/stable
