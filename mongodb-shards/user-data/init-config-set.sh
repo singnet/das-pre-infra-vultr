a0ffc7a52d58a04d9f0b349a84112e12b57fb6f7
levisingularity <rafael.levi@singularitynet.io>
2024-08-30 23:00:27 -0300

    add init config set script

---
+++ mongodb-shards/user-data/init-config-set.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+exec >/tmp/init-config-set.log 2>&1
+
+
+function get_replica_config() {
+    local public_ips=("$@")
+    local members="[]"
+    local config='{"_id": "config_repl", "members": []}'
+
+    for ((i=0; i<${#public_ips[@]}; i++)) {
+        local member="{\"_id\": $i, \"host\": \"${public_ips[i]}:28041\"}"
+        members=$(jq --argjson newMember "$member" '. + [$newMember]' <<<"$members")
+    }
+
+    config=$(jq --argjson members "$members" '.members += $members' <<<"$config")
+
+    echo "$config"
+}
+
+function get_replica_config_script() {
+    local rsconf
+    local replica_set_config
+    rsconf=$(get_replica_config "$@")
+    replica_set_config=$(cat <<EOF
+rsconf = ${rsconf};
+rs.initiate(rsconf);
+rs.status();
+EOF
+)
+
+    echo -e "$replica_set_config"
+}
+
+function setup() {
+    local rsconf
+
+    rsconf=$(get_replica_config_script "$@")
+
+    mongosh --port 28041 --eval "$rsconf" 
+}
+
+setup "$@"
